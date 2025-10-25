a1c4e388d2b601a20b7c142ab0c5dabe46101860
Joseph Aniag <jeiya5934@proton.me>
2025-10-25 15:15:07 +0800

    fix: some syntax styles were changed

--- src/DataStructures/LinkedListM.java
+++ src/DataStructures/LinkedListM.java
@@ -3,69 +3,74 @@ package DataStructures;
 public class LinkedListM {
     Node head;
 
-    public void insert(int data){
-        Node node =new Node(data);
-        if(head==null) head=node;
-        else{
-            Node curr=head;
-            while (curr.next!=null){
-                curr=curr.next;
-            }
-            curr.next=node;
-        }
-    }
-    public void insertStart(int data){
-        Node node=new Node(data);
-        node.next=head;
-        head=node;
-    }
-    public void insertAt(int index,int data){
-        Node node =new Node(data);
-        if(index==0) insertStart(data);
-        else{
-            Node curr=head;
-            for(int i=1;i<index;i++){
-                curr=curr.next;
-            } 
-            node.next=curr.next;
-            curr.next=node;
-        }
-    }
-    public void delete(int index){
-        Node curr=head;
-        if(index==0) head=head.next;
-        else {
-            for(int i=1;i<index;i++){
-                curr=curr.next;
-            }
-            curr.next=curr.next.next;
-        }
-    }
-    public void updateNode(int index, int data){
-        Node curr=head;
-        if(index ==0 ) head.data=data;
-        else{
-            for(int i=0;i<index;i++){
-                curr=curr.next;
-            } curr.data=data;
-        }
-    }
-    public void display(){
-        Node curr=head;
+    public void insert(int data) {
+        Node node = new Node(data);
         if (head == null) {
-        System.out.println("List is empty.");
-        return;
-    }
-        while(curr!=null){
-            System.out.print(curr.data+" ");
-            curr=curr.next;
+          head = node;
+        } else {
+            Node current = head;
+            while (current.next != null){
+                current = current.next;
+            }
+            current.next = node;
         }
     }
-    public int countNode(){
-        Node curr=head;
-        int count=0;
-        while(curr!=null){
-            curr =curr.next;
+    public void insertStart(int data) {
+        Node node = new Node(data);
+        node.next = head;
+        head = node;
+    }
+    public void insertAt(int index, int data) {
+        Node node = new Node(data);
+        if (index == 0) {
+            insertStart(data);
+        } else {
+            Node current = head;
+            for (int i = 1; i < index; i++) {
+                current = current.next;
+            } 
+            node.next = current.next;
+            current.next = node;
+        }
+    }
+    public void deleteAt(int index) {
+        Node current = head;
+        if (index == 0) {
+            head = head.next;
+        } else {
+            for (int i=1; i < index; i++){
+                current = current.next;
+            }
+            current.next = current.next.next;
+        }
+    }
+    public void updateNode(int index, int data) {
+        Node current = head;
+        if (index == 0) {
+            head.data = data;
+        } else {
+            for (int i = 1; i < index; i++) {
+                current = current.next;
+            }
+            current.data = data;
+        }
+    }
+    public void display() {
+        Node current = head;
+        if (head == null) {
+            System.out.println("List is empty.");
+            return;
+        }
+        while (current != null) {
+            System.out.println(current.data);
+            current = current.next;
+        }
+    }
+    public int countNode() {
+        Node current = head;
+        int count = 0;
+        while (current != null) {
+            current = current.next;
             count++;
         }
         return count;
@@ -74,8 +79,9 @@ public class LinkedListM {
 class Node {
     int data;
     Node next;
-    Node (int data){
-        this.data=data;
-        this.next=null;
+
+    Node (int data) {
+        this.data = data;
+        this.next = null;
     }
 }
